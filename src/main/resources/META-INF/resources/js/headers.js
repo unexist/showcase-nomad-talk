23b2acb610e8b7ea81bbf37ca2659c5c87338ef3
Christoph Kappel <christoph@unexist.dev>
2023-05-07 17:01:32 +0200

    Added script to dump headers

---
+++ src/main/resources/META-INF/resources/js/headers.js
@@ -0,0 +1,32 @@
+window.onload = function () {
+    var req = new XMLHttpRequest();
+
+    req.open('GET', document.location, false);
+    req.send(null);
+
+    /* Associate array to store all values */
+    var data = new Object();
+
+    /* Get all headers in one call and parse each item */
+    var headers = req.getAllResponseHeaders().toLowerCase();
+    var aHeaders = headers.split('\n');
+
+    for (var i= 0; i < aHeaders.length; i++) {
+        var thisItem = aHeaders[i];
+
+        var key = thisItem.substring(0, thisItem.indexOf(':'));
+        var value = thisItem.substring(thisItem.indexOf(':')+1);
+
+        data[key] = value;
+    }
+
+    var display = "";
+
+    for(var key in data) {
+        if ("" != key) {
+            display += "<b>" + key + "</b>: " + data[key];
+        }
+    }
+
+    document.getElementById("dump").innerHTML =  display;
+}
