e537de44b152b51f65858479addfcbbc8a6183a6
Christoph Kappel <christoph@unexist.dev>
2023-05-07 15:01:22 +0200

    Added hide mechanic

--- src/main/resources/META-INF/resources/js/clicks.js
+++ src/main/resources/META-INF/resources/js/clicks.js
@@ -1,6 +1,12 @@
 window.onclick = function(ev) {
     if (0 === ev.button) {
-        document.getElementById("next").click();
+        var elems= document.getElementsByClassName("hidden");
+
+        if (0 < elems.length) {
+            elems[0].classList.remove("hidden");
+        } else {
+            document.getElementById("next").click();
+        }
     } else {
         document.getElementById("prev").click();
     }
