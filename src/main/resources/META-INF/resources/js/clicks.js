074d0c17a2a1924e3a738388b5db7ed644c743f2
Christoph Kappel <christoph@unexist.dev>
2023-05-08 21:00:27 +0200

    Added handling for presenter

--- src/main/resources/META-INF/resources/js/clicks.js
+++ src/main/resources/META-INF/resources/js/clicks.js
@@ -1,13 +1,29 @@
-window.onclick = function(ev) {
-    if (0 === ev.button) {
-        var elems= document.getElementsByClassName("hidden");
+function handleNext() {
+    var elems= document.getElementsByClassName("hidden");
 
-        if (0 < elems.length) {
-            elems[0].classList.remove("hidden");
-        } else {
-            document.getElementById("next").click();
-        }
+    if (0 < elems.length) {
+        elems[0].classList.remove("hidden");
     } else {
-        document.getElementById("prev").click();
+        document.getElementById("next").click();
     }
-};
+}
+
+function handlePrev() {
+    document.getElementById("prev").click();
+}
+
+document.addEventListener("click", function(ev) {
+    if (0 === ev.button) {
+        handleNext();
+    } else if (2 === ev.button) {
+        handlePrev();
+    }
+})
+
+document.addEventListener("keydown", function(ev) {
+    switch (ev.keyCode) {
+        case 33: handleNext(); break; /* Next */
+        case 34: handlePrev(); break; /* Prev */
+        case 66: location.reload(); break; /* Square */
+    }
+});
